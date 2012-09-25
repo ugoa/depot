24060a557c5dc208267d66ba8504e9cd5c94349d
David Hu <hoodavy@gmail.com>
2012-09-25 21:35:58 +0800

    Finished Iteration J2: Translating the Storefront.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Depot::Application.routes.draw do
   get 'admin' => "admin#index"
 
   controller :sessions do
-    get 'login' => :new
-    post 'login' => :create
+    get    'login'  => :new
+    post   'login'  => :create
     delete 'logout' => :destroy
   end
 
